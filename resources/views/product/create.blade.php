0ea59a5086082f7fe0169c38d6fce9e77b92db78
Etinosa Obaseki <etinosaobaseki@gmail.com>
2019-12-18 20:52:47 +0000

    Show errors in create form

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -19,6 +19,15 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-8 offset-md-2">
+			@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
 			<form class="form" method="POST" action="{{ route('product.store') }}">
 				@csrf
 				<div class="form-group">
@@ -37,26 +46,26 @@
 						<label for="price">Price:</label>
 						<input type="number" class="form-control" placeholder="10000" name="price" id="price">
                     </div>
-                    <div class="form-group">
-                        <label for="is_physical">Is Physical:</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="is_physical" id="inlineRadio1" checked value=1>
-                            <label class="form-check-label" for="inlineRadio1">True</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="is_physical" id="inlineRadio2" value=0>
-                            <label class="form-check-label" for="inlineRadio2">False</label>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="category">Category:</label>
-                        <select name="category_id" class="custom-select">
+						<div class="form-group">
+							<label for="is_physical">Is Physical:</label>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="is_physical" id="inlineRadio1" checked value=1>
+								<label class="form-check-label" for="inlineRadio1">True</label>
+							</div>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="is_physical" id="inlineRadio2" value=0>
+								<label class="form-check-label" for="inlineRadio2">False</label>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="category">Category:</label>
+							<select name="category_id" class="custom-select">
                             @foreach($categories as $category)
                                 <option value={{ $category->id }}>{{ $category->name }}</option>
                             @endforeach
                         </select>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+						</div>
+						<button type="submit" class="btn btn-primary">Submit</button>
 			</form>
 		</div>
 	</div>
